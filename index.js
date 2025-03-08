b7e21cd8573c9b69217c292c07d0c5483fb06742
codebyalexx <a.legal.contact@gmail.com>
2025-03-08 17:11:14 +0100

    :sparkles: Added API service

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ const dotenv = require("dotenv");
 const { MinecraftServer } = require("./lib/mc-server.js");
 dotenv.config();
 new MinecraftServer();
+const api = require("./lib/api.js");
+api.listen();
